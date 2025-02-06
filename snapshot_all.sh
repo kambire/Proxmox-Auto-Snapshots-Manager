a5374456f369ab6cd5c25fc5db0c28bb44a12c2b
Cesar Augusto Benitez <kambire2007@gmail.com>
2025-02-06 14:58:26 -0300

    Update snapshot_all.sh

--- snapshot_all.sh
+++ snapshot_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Configuración
-SNAPSHOT_NAME="auto-snapshot"  # Nombre base del snapshot
+SNAPSHOT_NAME="auto-snapshot"  # Nombre base del snapshot lo puedes cambiar a tu gusto
 MAX_SNAPSHOTS=2  # Número máximo de snapshots a mantener
 
 # Obtener la lista de todas las máquinas virtuales (excluyendo plantillas, IDs >= 900)
-VM_IDS=$(qm list | awk '{if(NR>1 && $1 < 900) print $1}')  # Excluye IDs >= 900 y la cabecera
+VM_IDS=$(qm list | awk '{if(NR>1 && $1 < 900) print $1}')  # Excluye IDs >= 900 y la cabecera, mantener las plantillas por sobre estos id 900 y las vm por bajo de esta id
 
 # Función para crear snapshots
 create_snapshot() {
@@ -24,15 +24,10 @@ create_snapshot() {
 delete_old_snapshots() {
     local vm_id=$1
     echo "Eliminando snapshots antiguos para la máquina virtual $vm_id..."
-    
-    # Obtener la lista de snapshots ordenados por fecha (más recientes primero)
     snapshot_list=$(qm listsnapshot $vm_id | awk -v snapshot_name="$SNAPSHOT_NAME" '$0 ~ snapshot_name {print $2 " " $3 " " $4}' | sort -r)
-    
-    # Contar el número de snapshots
     snapshot_count=$(echo "$snapshot_list" | wc -l)
     
-    # Eliminar snapshots si hay más de MAX_SNAPSHOTS
-    if [ $snapshot_count -gt $MAX_SNAPSHOTS ]; then
+   if [ $snapshot_count -gt $MAX_SNAPSHOTS ]; then
         echo "$snapshot_list" | tail -n +$(($MAX_SNAPSHOTS + 1)) | while read -r snapshot; do
             snapshot_name=$(echo "$snapshot" | awk '{print $1}')
             echo "Eliminando snapshot $snapshot_name..."
